3c51dbceb17ac070851ebc76fdf66df7a14c3447
JoeMJr <josephmiddletonjr@outlook.com>
2026-01-11 22:01:53 -0500

    TestChar

---
+++ OPTest/OPCharacter.py
@@ -0,0 +1,13 @@
+
+# This will likely expand but that is a lot of work
+class OPCharacter:
+    # 
+    def __init__(self, name, age, race, home_loc, affiliations, job, current_loc, bounty, haki_prof, df, fighting_style, faction):
+        self.name = name
+        self.age = age
+        self.race = race
+        self.home_loc = home_loc
+        self.current_loc = current_loc
+        self.bounty = bounty
+        self.faction = faction
+        pass
